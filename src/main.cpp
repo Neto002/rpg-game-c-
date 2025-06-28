dd633d9172cf9a0524abdd0d9164b7ba1a57756c
Antonio Neto <antoniogfneto92@hotmail.com>
2025-06-28 13:39:39 -0300

    changing cmake's directory structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,49 @@
 #include <SFML/Graphics.hpp>
 
-void update(sf::RenderWindow &window);
-void draw(sf::RenderWindow &window);
-
 int main() {
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+
     // Create the main window (calls RenderWindow's constructor and assign an object to variable "window")
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "RPG Game");
+    sf::RenderWindow window(
+        sf::VideoMode({800, 600}),
+        "RPG Game",
+        sf::Style::Default,
+        sf::State::Windowed,
+        settings
+    );
+
+    sf::CircleShape shape(50.0f);
+    shape.setFillColor(sf::Color(250, 150, 100));
+    shape.setPosition(sf::Vector2f(100, 100));
+
+    sf::RectangleShape rectangleShape(sf::Vector2f(120, 50));
+    rectangleShape.setFillColor(sf::Color(250, 150, 100));
+    rectangleShape.setPosition(sf::Vector2f(250, 100));
+
+    sf::CircleShape octagon(50.0f, 8);
+    octagon.setFillColor(sf::Color(250, 150, 100));
+    octagon.setPosition(sf::Vector2f(400, 100));
 
     // Start the game loop
     while (window.isOpen()) {
-        update(window);
-        draw(window);
-    }
-}
-
-void update(sf::RenderWindow &window) {
-    // Process events
-    while (const std::optional event = window.pollEvent()) {
-        // Close window: exit
-        if (event->is<sf::Event::Closed>()) {
-            window.close();
+        // Process events
+        while (const std::optional event = window.pollEvent()) {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
         }
+
+        // Clear screen
+        window.clear();
+
+        // Drawing goes here
+        window.draw(shape);
+        window.draw(rectangleShape);
+        window.draw(octagon);
+
+        // Update the window
+        window.display();
     }
-}
-
-void draw(sf::RenderWindow &window) {
-    // Clear screen
-    window.clear();
-
-    // Drawing goes here
-
-    // Update the window
-    window.display();
 }
