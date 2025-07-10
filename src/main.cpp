0741d4e0e511616bd92121bea91f97652c970b58
Antonio Neto <antoniogfneto92@hotmail.com>
2025-07-10 13:56:37 -0300

    movement and changing player sprite based on direction

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include <cmath>
 #include <SFML/Graphics.hpp>
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
 
 int main() {
     sf::ContextSettings settings;
@@ -6,24 +10,30 @@ int main() {
 
     // Create the main window (calls RenderWindow's constructor and assign an object to variable "window")
     sf::RenderWindow window(
-        sf::VideoMode({800, 600}),
+        sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
         "RPG Game",
         sf::Style::Default,
         sf::State::Windowed,
         settings
     );
 
-    sf::CircleShape shape(50.0f);
-    shape.setFillColor(sf::Color(250, 150, 100));
-    shape.setPosition(sf::Vector2f(100, 100));
+    const sf::Texture playerTexture("assets/player/textures/spritesheet.png");
 
-    sf::RectangleShape rectangleShape(sf::Vector2f(120, 50));
-    rectangleShape.setFillColor(sf::Color(250, 150, 100));
-    rectangleShape.setPosition(sf::Vector2f(250, 100));
+    const int frameWidth = 32;
+    const int frameHeight = 32;
+    const int frameCount = 6;
+    const float frameDuration = 0.1f;
 
-    sf::CircleShape octagon(50.0f, 8);
-    octagon.setFillColor(sf::Color(250, 150, 100));
-    octagon.setPosition(sf::Vector2f(400, 100));
+    sf::Sprite playerSprite(playerTexture);
+    playerSprite.setScale(sf::Vector2f({2, 2}));
+    playerSprite.setOrigin(sf::Vector2f({frameWidth / 2.f, frameHeight / 2.f})); // Change sprite pivot to center
+    playerSprite.setPosition(sf::Vector2f({frameWidth, frameHeight}));
+
+    int currentFrame = 0;
+    float elapsed = 0.f;
+    sf::Clock clock;
+
+    int yIndex = 0;
 
     // Start the game loop
     while (window.isOpen()) {
@@ -35,13 +45,66 @@ int main() {
             }
         }
 
+        const float deltaTime = clock.restart().asSeconds();
+        elapsed += deltaTime;
+
+        // Changes frame if needed
+        if (elapsed > frameDuration) {
+            currentFrame = (currentFrame + 1) % frameCount;
+            elapsed = 0.f;
+
+            // Set texture rect
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+                yIndex = 5;
+                playerSprite.setScale(sf::Vector2f({2, 2}));
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+                yIndex = 3;
+                playerSprite.setScale(sf::Vector2f({2, 2}));
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+                yIndex = 4;
+                playerSprite.setScale(sf::Vector2f({-2, 2}));
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+                yIndex = 4;
+                playerSprite.setScale(sf::Vector2f({2, 2}));
+            }
+
+            playerSprite.setTextureRect(sf::IntRect({currentFrame * frameWidth, yIndex * frameHeight}, {frameWidth, frameHeight}));
+        }
+
+        // Player Movement Implementation
+        const float speed = 150.f;
+        sf::Vector2f movement(0.f, 0.f);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            movement.y -= 1.f;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            movement.y += 1.f;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            movement.x -= 1.f;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            movement.x += 1.f;
+        }
+
+        // Normalize diagonal movement (magnitude == 1)
+        if (movement.x != 0.f && movement.y != 0.f) {
+            movement /= std::sqrt(2.f);
+        }
+
+        playerSprite.move(movement * speed * deltaTime);
+
         // Clear screen
         window.clear();
 
-        // Drawing goes here
-        window.draw(shape);
-        window.draw(rectangleShape);
-        window.draw(octagon);
+        window.draw(playerSprite);
 
         // Update the window
         window.display();
